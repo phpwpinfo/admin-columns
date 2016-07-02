c807db72d927d2be704a197f253603a4c0c3a78c
WPindex <rohan@phpwpinfo.com>
2016-07-02 15:05:34 +0530

    Compatible with GitHub Updater

--- plugin.php
+++ plugin.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Custom Admin columns for Sorting and Filtering  
+Plugin Name: ABCD Custom Admin columns for Sorting and Filtering  
 Plugin URI: 
 Description: Add custom admin columns to post types to provide sorting and filtering through these extra columns
 Version: 1.0
@@ -9,6 +9,8 @@ Author: Wordpress Index
 Author URI: http://wordpressindex.com
 License: GNU General Public License 2.0 (GPL) http://www.gnu.org/licenses/gpl.html
 Text Domain:
+GitHub Plugin URI: https://github.com/phpwpinfo/admin-columns
+GitHub Branch:     master
 */
 
 include(plugin_dir_path( __FILE__ )."/wpi_custom_filter.php");
